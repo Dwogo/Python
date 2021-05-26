a492b48e773fa800d0703f460ce484b228633ef4
Dwog <philliptdavies@gmail.com>
2021-05-26 14:31:16 +1200

    Comments

--- hewo.py
+++ hewo.py
@@ -1,11 +1,12 @@
 import datetime
 
+# Time that gets displayed
 x = datetime.datetime.now()
-
-
-print(" The time is:")
+print("The time is:")
 print(x.strftime("%a %d %b %I %M %p"))
 
+
+# Name that the user chooses gets judged
 name = input("Please enter your name: ")
 
 if name == "Geoff":
