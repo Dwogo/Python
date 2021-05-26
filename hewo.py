0921c0a1a6a4bd80b93a151735c2f0c9a668abb8
Dwog <philliptdavies@gmail.com>
2021-05-26 14:08:06 +1200

    Geoff

--- hewo.py
+++ hewo.py
@@ -1,1 +1,12 @@
-print("Hewo World")
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%a %d %b %I %M %p"))
+
+name = input("Please enter your name: ")
+
+if name == "Geoff":
+    print("G'day Geoff")
+else:
+    print("Wait a minute you're not Geoff")
