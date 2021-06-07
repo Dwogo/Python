95cab6a80d92e303eb21962c4b77ef45edfcd2c8
Dwog <philliptdavies@gmail.com>
2021-06-08 00:20:00 +1200

    okie

--- okie.py
+++ okie.py
@@ -1,1 +1,16 @@
+print("Hi I'm here to help you in maths")
+Dimension = input("Is the shape 3D or 2D")
+
+if (Dimension == "3D" | Dimension == "3d"):
+    shape = input("What is the shape")
+    if (shape == "pyramid"):
+        y = int(input("What is the height"))
+        x = int(input("What is x"))
+        z = int(input("What is z"))
+        trongle = ((z * x)/2*y)/3
+        print(trongle)
+    elif (shape == "cube"):
+        y = int(input("What is the length of all sides/edges"))
+
+elif (Dimension == "2D" | Dimension | == "2d"):
 
