0203d250c8e7aa5f600edfc7b107728c713bd4df
Dwog <philliptdavies@gmail.com>
2021-06-09 02:30:05 +1200

    es

--- okie.py
+++ okie.py
@@ -11,6 +11,19 @@ if (Dimension == "3D" | Dimension == "3d"):
         print(trongle)
     elif (shape == "cube"):
         y = int(input("What is the length of all sides/edges"))
-
-elif (Dimension == "2D" | Dimension | == "2d"):
+        coobe = y*y*y
+        print(coobe)
+elif (Dimension == "2D" | Dimension == "2d"):
+    shape = input("What is the shape")
+    if (shape == "square" | shape == "Square"):
+        x = int(input("What is the hieght? "))
+        y = int(input("What is the length"))
+        sqee = x*y
+        print(sqee)
+    elif (shape == "triangle"):
+        x = int(input("What is the hieght? "))
+        y = int(input("What is the length"))
+        trongle2 = (x*y)/2
+        print(trongle2)
+    elif ()
 
