e6fc732eb1ccbf3374b95a3487f94988ab728c08
Vivo-dwog <57990308+Dwogo@users.noreply.github.com>
2021-06-20 15:34:22 -0700

    olympic values

---
+++ olympic.py
@@ -0,0 +1,12 @@
+
+
+guess = input("What is one of the 3 olympic values? ")
+
+# Turns the string to all lowercase
+guess = guess.lower()
+
+# 
+if (guess == "respect" or guess == "excellence" or guess == "friendship"):
+    print("correct")
+else:
+    print("incorrect")
