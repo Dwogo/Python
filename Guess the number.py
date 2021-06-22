18a41e89500c920cad0c916ff2f0d46833d24440
Vivo-dwog <57990308+Dwogo@users.noreply.github.com>
2021-06-21 17:53:06 -0700

    happo

--- Guess the number.py
+++ Guess the number.py
@@ -10,5 +10,5 @@ while (not number == 0):
     number = int(input("Guess a number "))
     guesses = guesses + 1
 
-print ("You guessed " + guesses + "times")
-print("The total sum is " + total)
+print ("You guessed " + str(guesses) + " times")
+print("The total sum is " + str(total))
