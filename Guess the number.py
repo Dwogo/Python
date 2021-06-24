1d812fa6e07652466cca2adae49c24996edbaf5d
Vivo-dwog <57990308+Dwogo@users.noreply.github.com>
2021-06-24 16:59:05 -0700

    Update Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -4,10 +4,11 @@ total = 0
 
 number = int(input("Guess a number "))
 guesses = guesses + 1
+total = total + number
 
 while (not number == 0):
-    total = total + number
-    number = int(input("Guess a number "))
+    number = input("Guess a number ")
+    total = total + int(number)
     guesses = guesses + 1
 
 print ("You guessed " + str(guesses) + " times")
